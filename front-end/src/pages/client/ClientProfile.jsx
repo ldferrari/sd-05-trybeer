d1d4e35d9e342eac9927225c97d291bb47806566
Mau <mauricio.sdjr@gmail.com>
2021-01-05 15:29:10 -0300

    setup App with routers

---
+++ front-end/src/pages/client/ClientProfile.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function ClientProfile() {
+ /*  const user = JSON.parse(localStorage.getItem('user')) || { email: '' };
+  // choice in order to make tests pass both on header, footer and profile
+  const emailUser = user.email;
+  const logOut = () => localStorage.clear(); */
+  return (
+    <div>
+      Hello World
+    </div>
+  );
+}
+
+export default ClientProfile;
