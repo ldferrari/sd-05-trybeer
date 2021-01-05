18c9d503ec1c52058e77d977fe3cb79538f19326
Christian Duessel <duessel@gmail.com>
2021-01-05 19:47:49 -0300

    estrutura da pagina de produtos iniciada

--- front-end/src/pages/Products/index.js
+++ front-end/src/pages/Products/index.js
@@ -1,23 +1,38 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 import './index.css';
+// import Header from '../../'
+
+import data from './data';
 
 const Products = () => {
   return (
-    <div className="productList">
-      <ul>
-        <li>
-          <div>
-            <img src={products.url} alt={products.name} />
-            <p data-testid="0-product-name">{product.name}</p>
-            <h4 data-testid="0-product-price">{product.price}</h4>
-            <button data-testid="0-product-minus">-</button>
-            <button data-testid="0-product-plus">+</button>
-          </div>
-        </li>
-      </ul>
+    <div className="Products">
+    {/* <Header>TryBeer</Header> */}
+      <div className="productList">
+        { data.products.map( product =>
+          <div className="card">
+            <img src={product.url_image} alt={product.name} data-testid={`${product.id-1}-product-img`}/>
+            <p data-testid={`${product.id-1}-product-name`}>{product.name}</p> 
+            <h4 data-testid={`${product.id-1}-product-price`}>R$ {product.price}</h4>
+            <div className="cardBottom">
+              <button data-testid={`${product.id-1}-product-minus`}>-</button>
+              <p data-testid={`${product.id-1}-product-qtd`}> soma qty</p>
+              <button data-testid={`${product.id-1}-product-plus`}>+</button>
+            </div>
+        </div>
+          )
+          }
+      </div>
+      <div className="checkoutBtn">
+        <Link to="/checkout" data-testid="checkout-bottom-btn" className="checkoutLink">
+          <p>Ver Carrinho</p>
+          <p data-testid="checkout-bottom-btn-value">Valor dentro do cart</p>
+        </Link>
+      </div>
+    {/* <Footer/> */}
     </div>
   )
-}
+};
 
 export default Products;
