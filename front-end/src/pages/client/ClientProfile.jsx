bc03c9676b1fe06e0ca99d8434f1b55f5fc10d18
Mau <mauricio.sdjr@gmail.com>
2021-01-05 18:59:20 -0300

    connection done, context setup done, started Profile page

--- front-end/src/pages/client/ClientProfile.jsx
+++ front-end/src/pages/client/ClientProfile.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+
+const handleChange = (e, name) => {
+  name = e.target.value;
+}
 
 function ClientProfile() {
- /*  const user = JSON.parse(localStorage.getItem('user')) || { email: '' };
+  const user = JSON.parse(localStorage.getItem('user')) || { email: '' };
   // choice in order to make tests pass both on header, footer and profile
   const emailUser = user.email;
-  const logOut = () => localStorage.clear(); */
+  const logOut = () => localStorage.clear();
   return (
     <div>
-      Hello World
+      <div data-testid="top-title">Meu perfil</div>
+      <label for="email">Email</label>
+      {/* campos email e name, o value vai puxar do login ou do localstorage */}
+      <input data-testid="profile-email-input" type="text" id="email" name="email" value="email do usuario" readonly />
+      <label for="name">Name</label>
+      <input data-testid="profile-name-input" type="text" id="name" name="name" value={name}/>
+      <button>
+        Salvar
+      </button>
     </div>
   );
 }
