512231378f1b44bec6d7db28adf921a501984aa0
Christian Duessel <duessel@gmail.com>
2021-01-05 21:44:58 -0300

    fixing slint for PR

--- front-end/src/pages/Products/index.js
+++ front-end/src/pages/Products/index.js
@@ -5,34 +5,35 @@ import './index.css';
 
 import data from './data';
 
-const Products = () => {
-  return (
-    <div className="Products">
+const Products = () =>
+  // return (
+  <div className="Products">
     {/* <Header>TryBeer</Header> */}
-      <div className="productList">
-        { data.products.map( product =>
-          <div className="card">
-            <img src={product.url_image} alt={product.name} data-testid={`${product.id-1}-product-img`}/>
-            <p data-testid={`${product.id-1}-product-name`}>{product.name}</p> 
-            <h4 data-testid={`${product.id-1}-product-price`}>R$ {product.price}</h4>
-            <div className="cardBottom">
-              <button data-testid={`${product.id-1}-product-minus`}>-</button>
-              <p data-testid={`${product.id-1}-product-qtd`}> soma qty</p>
-              <button data-testid={`${product.id-1}-product-plus`}>+</button>
-            </div>
+    <div className="productList">
+      { data.products.map(product =>
+        <div className="card" key={ product.name }>
+          <img src={ product.url_image } alt={ product.name } data-testid={`${ product.id - 1 }-product-img` }/>
+          <p data-testid={ `${ product.id - 1 }-product-name` }>{ product.name }</p>
+          <h4 data-testid={ `${ product.id - 1 }-product-price` }>
+            R$ { product.price }
+          </h4>
+          <div className="cardBottom">
+            <button type="button" data-testid={ `${ product.id - 1 }-product-minus` }>-</button>
+            <p data-testid={ `${ product.id - 1 }-product-qtd` }> soma qty</p>
+            <button type="button" data-testid={ `${ product.id - 1 }-product-plus` }>+</button>
+          </div>
         </div>
-          )
-          }
-      </div>
-      <div className="checkoutBtn">
-        <Link to="/checkout" data-testid="checkout-bottom-btn" className="checkoutLink">
-          <p>Ver Carrinho</p>
-          <p data-testid="checkout-bottom-btn-value">Valor dentro do cart</p>
-        </Link>
-      </div>
-    {/* <Footer/> */}
+      )},
     </div>
-  )
-};
+    <div className="checkoutBtn">
+      <Link to="/checkout" data-testid="checkout-bottom-btn" className="checkoutLink">
+        <p>Ver Carrinho</p>
+        <p data-testid="checkout-bottom-btn-value">Valor dentro do cart</p>
+      </Link>
+    </div>
+    {/* <Footer/> */}
+  </div>
+//  )
+// };
 
 export default Products;
