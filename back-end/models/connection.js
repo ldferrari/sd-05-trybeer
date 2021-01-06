9cf512ec0b8822e7772f64ef9bed0cf32ae18626
Andrevlc <andrevezi@gmail.com>
2021-01-06 17:58:25 -0300

    Testing xdev connection

--- back-end/models/connection.js
+++ back-end/models/connection.js
@@ -1,17 +1,41 @@
-const mysql = require('mysql2/promise');
+// const mysql = require('mysql2/promise');
 
 // require('dotenv').config({ path: `${__dirname}/../../.env` });
+// require('dotenv').config();
+
+// const config = {
+//   user: process.env.MYSQL_USER,
+//   password: process.env.MYSQL_PASSWORD,
+//   host: process.env.HOSTNAME,
+//   port: 33060,
+//   socketPath: '/var/run/mysqld/mysqld.sock',
+//   database: 'Trybeer',
+// };
+
+// const connection = mysql.createPool(config);
+
+// module.exports = connection;
+const mysqlx = require('@mysql/xdevapi');
 require('dotenv').config();
 
-const config = {
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  host: process.env.HOSTNAME,
-  port: 33060,
-  socketPath: '/var/run/mysqld/mysqld.sock',
-  database: 'Trybeer',
-};
-
-const connection = mysql.createPool(config);
-
+let schema = null;
+function connection() {
+  if (schema) return Promise.resolve(schema);
+  return mysqlx
+    .getSession({
+      user: process.env.MYSQL_USER,
+      password: process.env.MYSQL_PASSWORD,
+      host: process.env.HOSTNAME,
+      port: 33060,
+      schema: 'Trybeer',
+    })
+    .then((dbSchema) => {
+      schema = dbSchema;
+      return schema;
+    })
+    .catch((err) => {
+      console.error(err);
+      process.exit(1);
+    });
+}
 module.exports = connection;
