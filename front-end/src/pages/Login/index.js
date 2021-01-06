54bf3f030f62ccb3fd9c3b04d1964a5b08c4e876
pauloricardoz <trybe.przt@gmail.com>
2021-01-05 23:57:18 -0300

    login and register

--- front-end/src/pages/Login/index.js
+++ front-end/src/pages/Login/index.js
@@ -1,15 +1,90 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 // import { Redirect } from 'react-router-dom';
 import './index.css';
+// trocar por controler de login
+const getTokenAndData = async ({ email, password }) => ({
+  token: 'tokenDoido',
+  user: { email, password, role: 'admin' },
+});
+const saveToken = (token) => localStorage.setItem('token', token);
 
-const Login = () => {
+const Login = (props) => {
+  const [validEmail, setValidEmail] = useState(false);
+  const [validPassword, setValidPassword] = useState(false);
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const validationEmail = (value) => (/[A-Za-z0-9]+@[A-Za-z]+[A-z]*(\.\w{2,3})+/.test(value)
+    ? setValidEmail(true)
+    : setValidEmail(false));
+
+  const validationPassword = (value) => {
+    const minLength = 6;
+    return value.length >= minLength ? setValidPassword(true) : setValidPassword(false);
+  };
+
+  useEffect(() => {
+    validationEmail(email);
+  }, [email]);
+
+  useEffect(() => {
+    validationPassword(password);
+  }, [password]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const { token, user } = await getTokenAndData({ email, password });
+    saveToken(token);
+    if (user.role === 'admin') {
+      props.history.push('/admin/orders');
+    } else if (user.role === 'client') {
+      props.history.push('/products');
+    } else {
+      setValidEmail(true);
+      setValidEmail(false);
+    }
+  };
   return (
     <div>
-      <form>
-        <input/>
-        <input/>
-        <button>
-          Entrar
+      <form className="column-login">
+        <fieldset>
+
+          <label htmlFor="email">
+            Email:
+            <input
+              type="email"
+              name="email"
+              onChange={ ({ target: { value } }) => setEmail(value) }
+              data-testid="email-input"
+              value={ email }
+            />
+          </label>
+        </fieldset>
+        <fieldset>
+
+          <label htmlFor="password">
+            Password:
+            <input
+              type="password"
+              name="password"
+              onChange={ ({ target: { value } }) => setPassword(value) }
+              data-testid="password-input"
+              value={ password }
+            />
+          </label>
+        </fieldset>
+        <button data-testid="no-account-btn" type="button">
+          <Link to="/register">Ainda não tenho conta</Link>
+        </button>
+        <button
+          type="submit"
+          disabled={ !(validEmail && validPassword) }
+          onClick={ handleSubmit }
+          data-testid="signin-btn"
+        >
+          ENTRAR
         </button>
       </form>
     </div>
@@ -17,3 +92,5 @@ const Login = () => {
 };
 
 export default Login;
+
+Login.propTypes = { history: PropTypes.instanceOf(Object).isRequired };
--- front-end/src/routes.js
+++ front-end/src/routes.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import {
-  Login,
-} from './pages/';
+import { Login } from './pages';
+import Register from './pages/Register/Register';
 
 const Rotas = () => (
   <Switch>
-    <Route exact path="/" component={Login} />
+    <Route exact path="/login" component={ Login } />
+    <Route exact path="/register" component={ Register } />
   </Switch>
 );
 
