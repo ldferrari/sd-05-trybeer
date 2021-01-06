81dafc93cdde9e50ad4eaab06e801cfcbe2ce0f3
samueap <samueap@gmail.com>
2021-01-06 13:25:00 -0300

    setup-front

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,3 +1,32 @@
+// POSSIVEL FLUXO
+
+// import React from 'react';
+
+// import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+// import './App.css';
+// import Provider from './context/Provider';
+
+// // import Login from './pages/Login';
+
+// function App() {
+//   return (
+//     <div>
+//       <span>TryBeer</span>
+//       <Provider>
+//         <BrowserRouter>
+//           <Switch>
+//             <Route exact path="/login" component={Login} />
+
+//           </Switch>
+//         </BrowserRouter>
+//       </Provider>
+//     </div>
+//   );
+// }
+
+// export default App;
+
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
