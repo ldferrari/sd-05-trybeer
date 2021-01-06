19935afce5729c886869ee0cca6fc63d9e693841
César Alves <alvescesar@hotmail.com>
2021-01-06 18:41:39 -0300

    Login page back-end with JWT ready

--- back-end/models/userModel.js
+++ back-end/models/userModel.js
@@ -1,10 +1,14 @@
 const connection = require('./connection');
 
-const getByEmail = async (email) => connection.execute('SELECT * FROM users WHERE email = ?', [email]);
+const getByEmail = async (email) => {
+  const user = await connection.execute('SELECT * FROM users WHERE email = ?', [email]);
+  return user;
+};
 
-const createUser = async (name, email, password, role) => connection.execute(
-  ' INSERT INTO Trybeer.users (name, email, password, role) VALUES (?, ?, ?, ?) ',
-  [name, email, password, role],
-);
+const createUser = async (name, email, password, role) =>
+  connection.execute(
+    ' INSERT INTO Trybeer.users (name, email, password, role) VALUES (?, ?, ?, ?) ',
+    [name, email, password, role],
+  );
 
 module.exports = { getByEmail, createUser };
