1b918d1285c1f580f828619e231ae0ddb22f9f7e
ferrari <luisdiasferrari@gmail.com>
2021-01-05 21:02:48 -0300

    start MS

---
+++ front-end/src/components/client/MenuSuperior.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class MenuSuperior extends Component {
+  render() {
+    return (
+      <div>
+          dsaknono  
+      </div>
+    )
+  }
+}
