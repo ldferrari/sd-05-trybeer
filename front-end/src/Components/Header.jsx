42c9dea95153f049b7ad4cd4f43ca9ce16ff228c
Paulo-Dandrea <prog.dandrea@gmail.com>
2021-01-07 19:19:17 -0300

    nomes dos keys do estilo

--- front-end/src/Components/Header.jsx
+++ front-end/src/Components/Header.jsx
@@ -1,20 +1,20 @@
 import React from 'react';
 import titleForHeader from '../Helper/titleForHeader';
-
+// refatorar na estilização 
 const headerStyle = {
   background: '#100F0F',
   width: '375px',
   height: '86px',
 };
-
+// refatorar na estilização 
 const h1Style = {
-  'font-family': 'Roboto',
-  'font-size': '36px',
-  'font-style': 'normal',
-  'font-weight': '400',
-  'line-height': '42px',
-  'letter-spacing': '0em',
-  'text-align': 'left',
+  'fontFamily': 'Roboto',
+  'fontSize': '36px',
+  'fontStyle': 'normal',
+  'fontWeight': '400',
+  'lineHeight': '42px',
+  'letterSpacing': '0em',
+  'textAlign': 'left',
   'color': 'white',
 };
 
@@ -25,7 +25,9 @@ export default ({ pathname }) => {
   return (
     <div>
       <header style={headerStyle}>
-        <h1 style={h1Style}>{title}</h1>
+        <h1 data-testid="top-title" style={h1Style}>
+          {title}
+        </h1>
       </header>
     </div>
   );
