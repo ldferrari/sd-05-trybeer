71c653dd1372b4c6af46de6ba2a3f5e2eca8426e
ferrari <luisdiasferrari@gmail.com>
2021-01-07 18:20:40 -0300

    Menu

--- back-end/controllers/profileController.js
+++ back-end/controllers/profileController.js
@@ -1,14 +1,13 @@
 const { Router } = require('express');
 const rescue = require('express-rescue');
-const profileModel = require('../models/profileModel');
-
+/* const profileModel = require('../models/profileModel'); */
 
 const profileRouter = Router();
 
 profileRouter.get('/', rescue(async (_req, res) => {
-  const user = await profileModel.update()
+  /* const user = await profileModel.update(); */
 
-  return res.status(200).json('Hello World')
+  return res.status(200).json('Hello World');
 }));
 
 module.exports = profileRouter;
--- front-end/src/pages/client/ClientProfilePage.jsx
+++ front-end/src/pages/client/ClientProfilePage.jsx
@@ -12,9 +12,7 @@ export default function ClientProfilePage() {
   const [email] = useState('email@email.com');
   const [name, setName] = useState('Mau');
 
-  const handleChange = (e) => {
-      return validateName(setName(e.target.value));
-    };
+  const handleChange = (e) => validateName(setName(e.target.value));
 
   return (
     <div>
@@ -47,7 +45,7 @@ export default function ClientProfilePage() {
         data-testid="profile-save-btn"
         type="button"
         // segundo name vai vir da tela de login para comparar
-        disabled={ name ? true : false }
+        disabled
       >
         Salvar
       </button>
