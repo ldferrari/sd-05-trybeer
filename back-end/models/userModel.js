075c81e5f9c5bc40d8c7e0d6a8fb56a8b22ee1ef
César Alves <alvescesar@hotmail.com>
2021-01-08 18:14:48 -0300

    Fixed back-end eslint

--- back-end/models/userModel.js
+++ back-end/models/userModel.js
@@ -1,18 +1,15 @@
 const connection = require('./connection');
 
-const getAllUsers = async () => {
-  return connection.execute('SELECT * FROM users');
-};
+const getAllUsers = async () => connection.execute('SELECT * FROM users');
 
 const getByEmail = async (email) => {
   const user = await connection.execute('SELECT * FROM users WHERE email = ?', [email]);
   return user[0][0];
 };
 
-const createUser = async (name, email, password, role) =>
-  connection.execute(
-    ' INSERT INTO Trybeer.users (name, email, password, role) VALUES (?, ?, ?, ?) ',
-    [name, email, password, role],
-  );
+const createUser = async (name, email, password, role) => connection.execute(
+  'INSERT INTO Trybeer.users (name, email, password, role) VALUES (?, ?, ?, ?)',
+  [name, email, password, role],
+);
 
 module.exports = { getAllUsers, getByEmail, createUser };
