757778e01e994a858f02f8dee45e783dc111a85d
Paulo-Dandrea <prog.dandrea@gmail.com>
2021-01-08 09:56:27 -0300

    draft style

--- front-end/src/Components/SideBar.jsx
+++ front-end/src/Components/SideBar.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
-
+// estilo rascunho
 const sideBarStyle = {
-
+    position: 'absolute',
+    width: '249px',
+    height: '581px',
+    left: '0px',
+    top: '86px',
+    background: '#100F0F',
+    display: 'flex',
+    flexDirection: 'column'
 }
 
 function SideBar(props) {
   return (
-    <div style={{
-      position: 'absolute',
-      width: '249px',
-      height: '581px',
-      left: '0px',
-      top: '86px',
-      background: '#100F0F'
-    }}>
+    <div style={sideBarStyle}>
       <a href="">Produtos</a>
       <a href="">Meus Pedidos</a>
       <a href="">Meu Perfil</a>
