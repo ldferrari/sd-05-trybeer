4b58292fe1bd2f3d28140587699d68447a9aa48d
Giuliano Peixoto <giulianopeixoto@gmail.com>
2021-01-08 21:39:07 -0300

    teste fetch provider e ajuste ClientProductPage

--- front-end/src/pages/client/ClientProductPage.jsx
+++ front-end/src/pages/client/ClientProductPage.jsx
@@ -3,23 +3,24 @@ import { Link } from 'react-router-dom';
 import ProdCard from '../../components/client/ProductCard';
 // import ClientContext from '../../context/client/ClientContext';
 import productsApi from '../../services/client/api';
+import Menu from '../../components/client/Menu';
 
 const Products = () => {
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
-    const prodAPI = productsApi().then(response => setProducts(response));
+    productsApi().then(response => setProducts(response));
   }, []);
 
   // if (isLoading) return <div>Carregando...</div>;
 
   return (
     <div>
+      <Menu title="Trybeer" />
       <div>
         {products.map(product => (
           <ProdCard key={product.id} product={product} />
         ))}
-        ,
       </div>
       <div>
         <button type="button" data-testid="checkout-bottom-btn">
