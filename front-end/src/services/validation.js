fa26feaf224cb747445ff46e345872cb4f5093a9
Douglas <douglaza@gmail.com>
2021-01-08 21:44:24 -0300

    passwordLenght

--- front-end/src/services/validation.js
+++ front-end/src/services/validation.js
@@ -1,9 +1,10 @@
 const nameLength = 12;
-const passwordLength = 7;
+const passwordLength = 6;
 
-const validate = (email, password, name) => name.length > nameLength
-  && name.match(/^[a-zA-Z\u00C0-\u00FF\s]+$/)
-  && email.match(/\S+@\S+\.\S+/)
-  && !(password.length < passwordLength);
+const validate = (email, password, name) =>
+  name.length > nameLength &&
+  name.match(/^[a-zA-Z\u00C0-\u00FF\s]+$/) &&
+  email.match(/\S+@\S+\.\S+/) &&
+  !(password.length < passwordLength);
 
 export default validate;
