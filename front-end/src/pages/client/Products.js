6c440e93b06fae8852694f36e200a903de2628ad
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2021-01-08 22:06:56 -0300

    tried lots of things - call it a night

--- front-end/src/pages/client/Products.js
+++ front-end/src/pages/client/Products.js
@@ -7,8 +7,8 @@ import { getProducts } from '../../services/fetch';
 import '../../css/client/products.css';
 
 function Products() {
-  const [countProduct, setCountProduct] = useState(0);
   const [dataProducts, setDataProducts] = useState([]);
+  // const [totalPrice, setTotalPrice] = useState();
   // pbma: o count atualiza para todinhos
   const [isLogged, setIsLogged] = useState(true);
   // const { goCart, setGoCart } = useContext(TrybeerContext);
@@ -16,45 +16,39 @@ function Products() {
   // vai ou no context ou em props pra Checkout usado como componente filho
 
   useEffect(() => {
-    // getProducts().then((result) => console.log(result));
     getProducts().then((result) => setDataProducts(result));
     if (localStorage.getItem('user') === null) setIsLogged(false);
   }, []);
 
-  // mock substituido por verdadeiro DB de produtos do back
-  // const dbProducts = [
-  //   {
-  //     id: 1,
-  //     name: 'Skol Lata 250ml',
-  //     price: '2.20',
-  //     url_image: 'http://localhost:3001/images/Skol Lata 350ml.jpg',
-  //   },
-  //   {
-  //     id: 2,
-  //     name: 'Heineken 600ml',
-  //     price: '7.50',
-  //     url_image: 'http://localhost:3001/images/Heineken 600ml.jpg',
-  //   },
-  // ];
+  // useEffect(() => {
+  // }, [count])
 
-  const oneMore = () => {
-    setCountProduct(countProduct + 1);
-    // const num = 0;
-    // const countP = document.getElementById('price');
-    // const countText = document.createTextNode(num + 1); // retorna 1111
-    // countP.appendChild(countText);
-  };
-  const oneLess = (i) => {
-    if (countProduct >0) setCountProduct(countProduct - 1)
-  };
-  // FALTA - isso modifica todos ao mesmo tempo
+  // let num = 0;
+  // pbma: quando tem 3 num produto, o proximo começa a 4.
+
+  // const oneMore = (i) => {
+  //   // setTotalPrice(totalPrice+1*price)
+  //   num += 1;
+  //   // console.log(num);
+  //   const countP = document.getElementById(`${i}-price`);
+  //   countP.innerHTML=num;
+
+  // };
+
+  // const oneLess = (i) => {
+  //   // setTotalPrice(totalPrice+1*price)
+  //   num -= 1;
+  //   // console.log(num);
+  //   const countP = document.getElementById(`${i}-price`);
+  //   if (num>=0) return countP.innerHTML=num;
+  // };
 
   const createCart = () => {
     // setGoGart
   };
 
   const totalPrice = 3;
-  // achar como calcular
+  // forçando, achar como calcular
   
   return (
     <section>
@@ -62,34 +56,45 @@ function Products() {
       <ClientMenu title="TryBeer" />
       {/* {fetching && <p>Loading...</p>} */}
       {/* BACK - como ver que o fetch acabou? */}
-      {dataProducts.map((product, index) => (
+      {dataProducts.map((product, index) => {
+        let num = 0;
+        // tentei com forEach para colocar count e nem com return retornou renderizaçao
+        return (
         <div className="eachCard">
-          <p data-testid={`${index}-product-price`}>{new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(product.price)}</p>
-          {/* https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/NumberFormat/NumberFormat */}
-          {/* <p data-testid={`${index}-product-price`}>{product.price.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})}</p> */}
-          <img
-            data-testid={`${index}-product-img`}
-            src={`http://localhost:3001/images/${product.name}.jpg`}
-            alt=""
-            />
-          {/* FALTA: conectar a imagem com o images.tar.gz */}
-          <p key={product.id} data-testid={`${index}-product-name`}>{product.name}</p>
-          <div className="controlQty">
-          <button key={product.id} data-testid={`${index}-product-minus`} onClick={() => oneLess()}>
-            -
-          </button>
-          <p data-testid={`${index}-product-qtd`} id="price"></p>
-          <button key={product.id} data-testid={`${index}-product-plus`} onClick={() => oneMore()}>
-            +
-          </button>
+            <p data-testid={`${index}-product-price`}>{new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(product.price)}</p>
+            <img
+              data-testid={`${index}-product-img`}
+              src={`http://localhost:3001/images/${product.name}.jpg`}
+              alt=""
+              width="100"
+              height="60"
+              />
+            {/* FALTA: conectar a imagem com o images.tar.gz */}
+            <p key={product.id} data-testid={`${index}-product-name`}>{product.name}</p>
+            <div className="controlQty">
+            <button key={product.id} data-testid={`${index}-product-minus`}
+            // onClick={() => oneLess(index)}
+            onClick={() => num-=1}
+            >
+              -
+            </button>
+            <p data-testid={`${index}-product-qtd`} id={`${index}-price`}>{num}</p>
+            <button key={product.id} data-testid={`${index}-product-plus`}
+            // onClick={() => oneMore(index)}
+            onClick={() => console.log(num+=1)}
+            >
+              +
+            </button>
+            </div>
           </div>
-        </div>
-      ))}
+        )
+        })
+      }
       </div>
       <Link to="/checkout">
-        <button data-testid="checkout-bottom-btn" onClick={() => createCart()}>
+        <button data-testid="checkout-bottom-btn" onClick={ () => createCart() }>
           Ver Carrinho
-          <p data-testid="checkout-bottom-btn-value">{new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(totalPrice)}</p>
+          <p data-testid="checkout-bottom-btn-value">{ new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(totalPrice)}</p>
         </button>
       </Link>
       {!isLogged && <Redirect to="/login" />}
@@ -98,3 +103,15 @@ function Products() {
 }
 
 export default Products;
+
+// {!isFetching && <section className="list-orders">
+//         {ordersAdmin && ordersAdmin.map((result, index) => <CardOrdersAdmin orders={result} index={index} key={index} />)}
+//       </section>}
+
+//  fonte sobre currency https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/NumberFormat/NumberFormat
+
+// backlog 08.01 noite
+// - achar logica para contagens individuais
+// - a partir desta logica, conseguir o totalPrice
+// - imagens do .gz
+// - sincronizar com card da tela Checkout (carrinho)
