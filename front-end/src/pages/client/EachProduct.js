e610d85944e0a05a63b8ae591ca8ee5b58ed83ca
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2021-01-08 23:02:50 -0300

    Products 10/11 yeah

---
+++ front-end/src/pages/client/EachProduct.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { useState, useContext, useEffect } from 'react';
+import TrybeerContext from '../../context/TrybeerContext';
+
+function EachProduct(props) {
+  const { product, index, key } = props;
+  const [countProduct, setCountProduct] = useState(0);
+  const { totalPrice, setTotalPrice } = useContext(TrybeerContext);
+
+
+  const oneLess = () => {
+    if (!(countProduct <= 0)) {
+      setCountProduct(countProduct - 1);
+      setTotalPrice(totalPrice - product.price);
+    }
+  };
+  const oneMore = () => {
+    setCountProduct(countProduct + 1);
+    setTotalPrice(totalPrice + Number(product.price))
+  };
+
+  return (
+    <div className="eachCard">
+      <p data-testid={`${index}-product-price`}>
+        {new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(
+          product.price
+        )}
+      </p>
+      <img
+        data-testid={`${index}-product-img`}
+        src={`http://localhost:3001/images/${product.name}.jpg`}
+        alt=""
+        width="100"
+        height="60"
+      />
+      {/* FALTA: conectar a imagem com o images.tar.gz */}
+      <p key={product.id} data-testid={`${index}-product-name`}>
+        {product.name}
+      </p>
+      <div className="controlQty">
+        <button key={product.id} data-testid={`${index}-product-minus`} onClick={() => oneLess()}>
+          -
+        </button>
+        <p data-testid={`${index}-product-qtd`} id={`${index}-price`}>
+          {countProduct}
+        </p>
+        <button key={product.id} data-testid={`${index}-product-plus`} onClick={() => oneMore()}>
+          +
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default EachProduct;
--- front-end/src/pages/client/Products.js
+++ front-end/src/pages/client/Products.js
@@ -3,98 +3,43 @@ import { Link, Redirect } from 'react-router-dom';
 import { useState, useContext, useEffect } from 'react';
 import ClientMenu from '../../components/client/ClientMenu';
 import { getProducts } from '../../services/fetch';
-// import TrybeerContext from '../../context/TrybeerContext';
+import TrybeerContext from '../../context/TrybeerContext';
+import EachProduct from './EachProduct';
 import '../../css/client/products.css';
 
 function Products() {
   const [dataProducts, setDataProducts] = useState([]);
-  // const [totalPrice, setTotalPrice] = useState();
-  // pbma: o count atualiza para todinhos
   const [isLogged, setIsLogged] = useState(true);
-  // const { goCart, setGoCart } = useContext(TrybeerContext);
-  // "Caso a pessoa atualize o browser, o carrinho deve ser mantido;"
-  // vai ou no context ou em props pra Checkout usado como componente filho
+  const { totalPrice } = useContext(TrybeerContext);
 
   useEffect(() => {
     getProducts().then((result) => setDataProducts(result));
     if (localStorage.getItem('user') === null) setIsLogged(false);
   }, []);
 
-  // useEffect(() => {
-  // }, [count])
-
-  // let num = 0;
-  // pbma: quando tem 3 num produto, o proximo começa a 4.
-
-  // const oneMore = (i) => {
-  //   // setTotalPrice(totalPrice+1*price)
-  //   num += 1;
-  //   // console.log(num);
-  //   const countP = document.getElementById(`${i}-price`);
-  //   countP.innerHTML=num;
-
-  // };
-
-  // const oneLess = (i) => {
-  //   // setTotalPrice(totalPrice+1*price)
-  //   num -= 1;
-  //   // console.log(num);
-  //   const countP = document.getElementById(`${i}-price`);
-  //   if (num>=0) return countP.innerHTML=num;
-  // };
-
-  const createCart = () => {
-    // setGoGart
-  };
-
-  const totalPrice = 3;
-  // forçando, achar como calcular
-  
   return (
     <section>
-      <div className="productCards">
       <ClientMenu title="TryBeer" />
-      {/* {fetching && <p>Loading...</p>} */}
-      {/* BACK - como ver que o fetch acabou? */}
-      {dataProducts.map((product, index) => {
-        let num = 0;
-        // tentei com forEach para colocar count e nem com return retornou renderizaçao
-        return (
-        <div className="eachCard">
-            <p data-testid={`${index}-product-price`}>{new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(product.price)}</p>
-            <img
-              data-testid={`${index}-product-img`}
-              src={`http://localhost:3001/images/${product.name}.jpg`}
-              alt=""
-              width="100"
-              height="60"
-              />
-            {/* FALTA: conectar a imagem com o images.tar.gz */}
-            <p key={product.id} data-testid={`${index}-product-name`}>{product.name}</p>
-            <div className="controlQty">
-            <button key={product.id} data-testid={`${index}-product-minus`}
-            // onClick={() => oneLess(index)}
-            onClick={() => num-=1}
-            >
-              -
-            </button>
-            <p data-testid={`${index}-product-qtd`} id={`${index}-price`}>{num}</p>
-            <button key={product.id} data-testid={`${index}-product-plus`}
-            // onClick={() => oneMore(index)}
-            onClick={() => console.log(num+=1)}
-            >
-              +
-            </button>
-            </div>
-          </div>
-        )
-        })
-      }
+      <div className="productCards">
+        {/* {fetching && <p>Loading...</p>} */}
+        {/* BACK - como ver que o fetch acabou? */}
+        {dataProducts.map((product, index) => (
+          <EachProduct product={product} index={index} key={index} />
+        ))}
       </div>
       <Link to="/checkout">
-        <button data-testid="checkout-bottom-btn" onClick={ () => createCart() }>
+        <button
+          data-testid="checkout-bottom-btn"
+          disabled={totalPrice===0}
+        // onClick={() => createCart()}
+        >
           Ver Carrinho
-          <p data-testid="checkout-bottom-btn-value">{ new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(totalPrice)}</p>
+          <p data-testid="checkout-bottom-btn-value">
+            {new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(
+              totalPrice
+            )}
+            {/* fonte sobre currency https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/NumberFormat/NumberFormat */}
+          </p>
         </button>
       </Link>
       {!isLogged && <Redirect to="/login" />}
@@ -104,14 +49,6 @@ function Products() {
 
 export default Products;
 
-// {!isFetching && <section className="list-orders">
-//         {ordersAdmin && ordersAdmin.map((result, index) => <CardOrdersAdmin orders={result} index={index} key={index} />)}
-//       </section>}
-
-//  fonte sobre currency https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/NumberFormat/NumberFormat
-
 // backlog 08.01 noite
-// - achar logica para contagens individuais
-// - a partir desta logica, conseguir o totalPrice
 // - imagens do .gz
-// - sincronizar com card da tela Checkout (carrinho)
+// - mandar para card da tela Checkout (carrinho)
