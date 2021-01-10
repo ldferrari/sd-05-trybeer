7c38ea42ac66946707bd224710366dac685ed763
Sidnei Ramos <sidneiramos02@gmail.com>
2021-01-10 20:10:08 -0300

    Copiando HomePAge do Login

--- front-end/src/pages/general/HomePage.jsx
+++ front-end/src/pages/general/HomePage.jsx
@@ -1,16 +1,68 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, Redirect } from 'react-router-dom';
+import loginData from '../../services/general/fetchLoginData';
+import validateEmail from '../../services/general/validateEmail';
+import validatePassword from '../../services/general/validatePassword';
 
-export default function HomePage() {
+export default function LoginPage() {
+  const [emailValidate, setEmailValidate] = useState(false);
+  const [passwordValidate, setPasswordValidate] = useState(false);
+  const [userData, setUserData] = useState({
+    email: '',
+    password: '',
+    role: '',
+  });
+
+  const login = async (data) => {
+    const usuario = await loginData(data);
+    setUserData({ ...userData, role: usuario.role });
+  };
+
+  if (userData.role === 'administrator') return <Redirect to="/admin/orders" />;
+  if (userData.role === 'client') return <Redirect to="/products" />;
   return (
     <div>
-      <div>
-        <button type="button" data-testid="no-account-btn">
-          <Link to="/register">
-            Ainda não tenho conta
-          </Link>
-        </button>
-      </div>
+      <label htmlFor="email">
+        Email
+        <input
+          type="text"
+          id="email"
+          data-testid="email-input"
+          onChange={ (event) => {
+            if (validateEmail(event.target.value)) {
+              setEmailValidate(true);
+              setUserData({ ...userData, email: event.target.value });
+            }
+          } }
+        />
+      </label>
+      <label htmlFor="senha">
+        Senha
+        <input
+          type="password"
+          id="senha"
+          data-testid="password-input"
+          onChange={ (event) => {
+            if (validatePassword(event.target.value)) {
+              setPasswordValidate(true);
+              setUserData({ ...userData, password: event.target.value });
+            }
+          } }
+        />
+      </label>
+      <button
+        type="button"
+        data-testid="signin-btn"
+        disabled={ !emailValidate || !passwordValidate }
+        onClick={ () => {
+          login(userData);
+        } }
+      >
+        ENTRAR
+      </button>
+      <button type="button" data-testid="no-account-btn">
+        <Link to="/register">Ainda não tenho conta</Link>
+      </button>
     </div>
   );
 }
