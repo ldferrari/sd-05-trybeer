d46a4e726eb91616880e9866b6a740d97d2e2443
ferrari <luisdiasferrari@gmail.com>
2021-01-10 20:30:56 -0300

    fixed

--- front-end/src/components/client/ProductCard.jsx
+++ front-end/src/components/client/ProductCard.jsx
@@ -1,27 +1,31 @@
-import React, { useEffect, useContext, useState } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 // import ClientContext from '../../context/client/ClientContext';
-import QuantityButton from '../../components/client/QuantityButton';
+import QuantityButton from './QuantityButton';
 
 const ProdCard = (props) => {
-  
   const { product } = props;
-
   return (
     <div key={ product.id }>
-      <img 
+      <img
         src={ product.url_image }
         data-testid={ `${product.id}-product-img` }
-        alt={product.name}
+        alt={ product.name }
       />
       <div data-testid={ `${product.id}-product-name` }>
         { product.name }
       </div>
       <div data-testid={ `${product.id}-product-price` }>
-        R$ { product.price }               
+        R$
+        { product.price }
       </div>
       <QuantityButton />
     </div>
-  )
+  );
 }
+
+ProdCard.propTypes = {
+  product: PropTypes.object.isRequired,
+};
 
 export default ProdCard;
