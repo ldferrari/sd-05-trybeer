bf7389b98939b929edca9a4a70f26556b796e035
pauloricardoz <trybe.przt@gmail.com>
2021-01-11 19:49:05 -0300

    changes in the back to pass the tests of registration, login and products

--- back-end/Controllers/userController.js
+++ back-end/Controllers/userController.js
@@ -18,21 +18,23 @@ register.post('/', async (req, res) => {
     if (!/^[A-Za-z \s]{12,}$/.test(name)) {
       return res.status(401).json({
         message: 'O nome deve conter, no mínimo, 12 letras, sem números ou caracteres especiais',
+        ok:false,
       });
     }
 
     if (!validateEmail) {
       return res.status(400).json({
         message: 'Email inválido. Um email válido possui o formato <nome>@<domínio>',
+        ok:false,
       });
     }
 
     if (password.length < 6) {
-      return res.status(401).json({ message: 'Email ou senha incorreto.' });
+      return res.status(401).json({ message: 'Email ou senha incorreto.', ok:false });
     }
     const newUser = await service.create(name, email, password, role);
     if (newUser.error) {
-      return res.status(newUser.statusCode).json({ message: newUser.message });
+      return res.status(newUser.statusCode).json({ message: newUser.message, ok:false });
     }
     const token = createToken({
       email,
@@ -40,10 +42,10 @@ register.post('/', async (req, res) => {
       iss: 'post_api',
     });
 
-    res.status(201).json({ user: { name, email, role }, token });
+    res.status(201).json({ user: { name, email, role }, token, ok:true });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: 'Algo deu errado.' });
+    res.status(500).json({ message: 'Algo deu errado.', ok:false });
   }
 });
 
