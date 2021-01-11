294cd675713e86bbb2baea768e471e4ad84931ae
Douglas <douglaza@gmail.com>
2021-01-11 19:42:03 -0300

    parenthesis

--- back-end/services/registerService.js
+++ back-end/services/registerService.js
@@ -10,10 +10,10 @@ class Error extends Error {
 const createUser = async (name, email, password, role) => {
   const thisEmailAlreadyExists = await userModel.getByEmail(email);
   if (thisEmailAlreadyExists) {
-    throw new Error ({ code: 'invalid_email', message: 'E-mail already in database.' });
+    throw new Error({ code: 'invalid_email', message: 'E-mail already in database.' });
   }
   if (typeof name !== 'string' || typeof email !== 'string' || password.length < 6) {
-    throw new Error ({ code: 'invalid_data', message: 'Registration failed!' });
+    throw new Error({ code: 'invalid_data', message: 'Registration failed!' });
   }
   const newUser = await userModel.createUser(name, email, password, role);
   return newUser;
