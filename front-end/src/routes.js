31e3b0679ff935e553121b5f393d972bf599c644
Christian Duessel <duessel@gmail.com>
2021-01-11 18:21:12 -0300

    checkout page

--- front-end/src/routes.js
+++ front-end/src/routes.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import { Login, Profile, Products } from './pages';
+import Checkout from './pages/checkout';
 import Register from './pages/Register/Register';
 
 const Rotas = () => (
@@ -10,6 +11,7 @@ const Rotas = () => (
     <Route path="/profile" component={ Profile } />
     <Route path="/products" component={ Products } />
     <Route exact path="/register" component={ Register } />
+    <Route path="/checkout" component={ Checkout } />
   </Switch>
 );
 
