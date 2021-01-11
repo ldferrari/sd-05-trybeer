e20d19f44217edee6e373e41c5fc34418b6d2c9c
ferrari <luisdiasferrari@gmail.com>
2021-01-11 16:09:44 -0300

    menu done

--- front-end/src/components/client/Menu.jsx
+++ front-end/src/components/client/Menu.jsx
@@ -22,19 +22,19 @@ export default function Menu(props) {
         <input type="checkbox" id="check" onChange={ () => menuChecked() } />
       </label>
       <h1 className="topTitle" data-testid="top-title">{title}</h1>
-      {isVisible && <div className="side-menu-container">
-        <nav className="menuLateral">
-          <div className="menuButton">
-            <a href="/products" data-testid="side-menu-item-products" className="buttonLateral">Produtos</a>
-            <a href="/orders" data-testid="side-menu-item-my-orders" className="buttonLateral">Meus Pedidos</a>
-            <a href="/profile" data-testid="side-menu-item-my-profile" className="buttonLateral">Meu Perfil</a>
-          </div>
-          <div className="menuButton textCenter">
-            <a href="/login" data-testid="side-menu-item-logout" className="buttonLateral">Sair</a>
-          </div>
-        </nav>
-      </div>
-      }
+      {isVisible && 
+        <div className="side-menu-container">
+          <nav className="menuLateral">
+            <div className="menuButton">
+              <a href="/products" data-testid="side-menu-item-products" className="buttonLateral">Produtos</a>
+              <a href="/orders" data-testid="side-menu-item-my-orders" className="buttonLateral">Meus Pedidos</a>
+              <a href="/profile" data-testid="side-menu-item-my-profile" className="buttonLateral">Meu Perfil</a>
+            </div>
+            <div className="menuButton textCenter">
+              <a href="/login" data-testid="side-menu-item-logout" className="buttonLateral">Sair</a>
+            </div>
+          </nav>
+        </div>}
     </header>
   );
 }
