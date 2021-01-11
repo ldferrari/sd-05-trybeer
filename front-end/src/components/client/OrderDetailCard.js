5983dc809354c4f5940a563cc3e05673c151d2ff
virginia-duca <virginiambd.bio@gmail.com>
2021-01-11 19:30:30 -0300

    formatei com prettier orderDetail e orderDetailCard

--- front-end/src/components/client/OrderDetailCard.js
+++ front-end/src/components/client/OrderDetailCard.js
@@ -1,24 +1,28 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { useEffect } from 'react';
-import { getProductById } from '../../services/fetch'
+import { getProductById } from '../../services/fetch';
 
 export default function OrderCard({ item, index }) {
-    const [productDetail, setProductDetail] = useState([])
+  const [productDetail, setProductDetail] = useState([]);
 
-    useEffect(() => {
-    getProductById(item.product_id).then((result) => setProductDetail(...result))
-    },[])
+  useEffect(() => {
+    getProductById(item.product_id).then((result) =>
+      setProductDetail(...result)
+    );
+  }, []);
 
   return (
     <div>
-        <p data-testid={`${index}-product-qtd`}>{item.quantity} - </p>
-        {console.log(productDetail)}
-        <p data-testid={`${index}-product-name`}>{productDetail.name}</p>
-        <p data-testid={`${index}-product-total-value`}>R$ {productDetail.price}</p>
+      <p data-testid={`${index}-product-qtd`}>{item.quantity} - </p>
+      {console.log(productDetail)}
+      <p data-testid={`${index}-product-name`}>{productDetail.name}</p>
+      <p data-testid={`${index}-product-total-value`}>
+        R$ {productDetail.price}
+      </p>
     </div>
   );
-};
+}
 
 OrderCard.propTypes = {
   order: PropTypes.object.isRequired,
--- front-end/src/pages/client/OrdersDetails.js
+++ front-end/src/pages/client/OrdersDetails.js
@@ -1,37 +1,37 @@
 import React, { useEffect, useState } from 'react';
-import {withRouter} from 'react-router-dom'
-import ClientMenu from '../../components/client/ClientMenu'
-import { getSaleById, getSaleDetails } from '../../services/fetch'
-import OrderDetailCard from '../../components/client/OrderDetailCard'
+import { withRouter } from 'react-router-dom';
+import ClientMenu from '../../components/client/ClientMenu';
+import { getSaleById, getSaleDetails } from '../../services/fetch';
+import OrderDetailCard from '../../components/client/OrderDetailCard';
 
-function OrdersDetails({ history, location: { pathname } } ) {
-  const [orderId, setOrderId] = useState('')
-  const [orderInfo, setOrderInfo] = useState([])
-  const [date1, setDate1] = useState('21-01')
-  const [productsDetails, setProductsDetails] = useState([])
-  
+function OrdersDetails({ history, location: { pathname } }) {
+  const [orderId, setOrderId] = useState('');
+  const [orderInfo, setOrderInfo] = useState([]);
+  const [date1, setDate1] = useState('21-01');
+  const [productsDetails, setProductsDetails] = useState([]);
+
   const separateString = (string, separator) => {
-    const arrayStrings = string.split(separator)
-    return arrayStrings
-  }
+    const arrayStrings = string.split(separator);
+    return arrayStrings;
+  };
 
   const getDate = (dateFormat) => {
     if (typeof dateFormat === 'string') {
       let date = dateFormat;
       date = date.substring(0, 10);
       date = date.split('-');
-      console.log(date)
-      return `${date[1]}/${date[2]}`
+      console.log(date);
+      return `${date[1]}/${date[2]}`;
     }
-  }
+  };
 
   useEffect(() => {
-    const array = separateString(pathname, "/")
-    console.log(array)
-    setOrderId(array[2])
-    getSaleById(array[2]).then(result => setOrderInfo(...result))
-    getSaleDetails(array[2]).then(result => setProductsDetails(result))
-  }, [])
+    const array = separateString(pathname, '/');
+    console.log(array);
+    setOrderId(array[2]);
+    getSaleById(array[2]).then((result) => setOrderInfo(...result));
+    getSaleDetails(array[2]).then((result) => setProductsDetails(result));
+  }, []);
 
   return (
     <div>
@@ -40,10 +40,14 @@ function OrdersDetails({ history, location: { pathname } } ) {
       <div data-testid="order-number">Pedido {orderInfo.id}</div>
       <div data-testid="order-date">{getDate(orderInfo.sale_date)}</div>
       <div>
-        {productsDetails.map((item, index) => <OrderDetailCard key={index} item={item} index={index}/>)}
+        {productsDetails.map((item, index) => (
+          <OrderDetailCard key={index} item={item} index={index} />
+        ))}
       </div>
       {console.log(productsDetails)}
-      <div data-testid="order-total-value">Total: R$ {orderInfo.total_price}</div>
+      <div data-testid="order-total-value">
+        Total: R$ {orderInfo.total_price}
+      </div>
     </div>
   );
 }
