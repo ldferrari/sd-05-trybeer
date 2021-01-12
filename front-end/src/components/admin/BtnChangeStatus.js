f84058bc0d04ce6a1dff07297e32718b22d3200a
Lucascv <lucascv333@gmail.com>
2021-01-12 14:51:27 -0300

    finalizando btn change status

--- front-end/src/components/admin/BtnChangeStatus.js
+++ front-end/src/components/admin/BtnChangeStatus.js
@@ -4,7 +4,7 @@ import TrybeerContext from '../../context/TrybeerContext';
 import PropTypes from 'prop-types';
 
 export default function BtnStatus({ id }) {
-  const { status } = useContext(TrybeerContext);
+  const { status, setStatus } = useContext(TrybeerContext);
 
   if (!status || status === "Entregue") return <div></div>;
 
@@ -12,7 +12,7 @@ export default function BtnStatus({ id }) {
     <button
       type="button"
       data-testid="mark-as-delivered-btn"
-      onClick={() => closeSale(id).then(window.location.reload(true))}
+      onClick={() => closeSale(id).then(setStatus('Entregue'))}
     >
       Marcar como entregue
     </button>
