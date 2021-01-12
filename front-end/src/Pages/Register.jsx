2f2b251e2d92533d2aa2f9c07d3408d004a4a4a8
Paulo-Dandrea <prog.dandrea@gmail.com>
2021-01-12 16:52:41 -0300

    register validations working properly - now it needs to connect to database

--- front-end/src/Pages/Register.jsx
+++ front-end/src/Pages/Register.jsx
@@ -9,9 +9,9 @@ const Register = () => {
   const [isDisabled, isSetDisabled] = useState(true);
 
   function validate() {
-    const validName = (name || '').length > 3;
-    const validEmail = (/^\w+([.-]?\w+)@\w+([.-]?\w+)(.\w{2,3})+$/).test(email);
-    const validSenha = (/^[^W_]{7,100}$/).test(password);
+    const validName =  /^[a-zA-Z ]{12}[a-zA-Z ]*$/.test(name);
+    const validEmail = /^\w+([.-]?\w+)@\w+([.-]?\w+)(.\w{2,3})+$/.test(email);
+    const validSenha = /^[^W_]{7,100}$/.test(password);
     if (validName && validEmail && validSenha) isSetDisabled(false);
     else isSetDisabled(true);
   }
@@ -24,29 +24,39 @@ const Register = () => {
     <div className="container-main" id="Register">
       <div className="container-page">
         <h1>Register</h1>
+        <label htmlFor="name">Nome</label>
         <Input
-          test="name-input"
+          test="signup-name"
+          id="name"
           placeholder="Digite seu nome"
           onChange={(e) => setName(e.target.value)}
         />
+        <label htmlFor="email">Email</label>
         <Input
-          test="email-input"
+          test="signup-email"
+          id="email"
           placeholder="Digite seu e-mail"
           onChange={(e) => setEmail(e.target.value)}
         />
+        <label htmlFor="password">Senha</label>
         <Input
           type="password"
-          test="password-input"
+          test="signup-password"
+          id="password"
           placeholder="Digite sua senha"
           onChange={(e) => setPassword(e.target.value)}
         />
+        <label htmlFor="quero-vender">Quero Vender</label>
         <input
           type="checkbox"
+          id="quero-vender"
           data-testid="signup-seller"
-          onChange={ ({ target: { checked } }) => { setIsSeller(checked) } }
+          onChange={({ target: { checked } }) => {
+            setIsSeller(checked);
+          }}
         />
-        <button disabled={isDisabled}>
-          Entrar
+        <button disabled={isDisabled} data-testid="signup-btn">
+          Cadastrar
         </button>
       </div>
     </div>
