cb7200b06338700c5c3e4c05140af3d12c8e7254
luca <luucadecastro@gmail.com>
2021-01-12 03:32:41 -0300

    css login

--- front-end/src/pages/Login.js
+++ front-end/src/pages/Login.js
@@ -3,11 +3,11 @@ import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import TryBeerContext from '../context/TryBeerContext';
 import fetchLogin from '../services/ApiTrybeer';
+import '../style/Login.css';
+const logo = require('../logo.jpg');
 
 const Login = ({ history }) => {
-  const {
-    email, setEmail, password, setPassword,
-  } = useContext(TryBeerContext);
+  const { email, setEmail, password, setPassword } = useContext(TryBeerContext);
   const RegEx = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   const validEmail = RegEx.test(String(email).toLowerCase());
   const passwordLength = 6;
@@ -21,38 +21,47 @@ const Login = ({ history }) => {
 
   return (
     <section>
-      <form>
-        <label htmlFor="email-input">
+      <form className='login-container'>
+      <img src={logo} alt={'garrafamóvel'} className="logo"/>       
+        <span className='login-container title'>TryBeer</span>
+        <label htmlFor="email-input" className='login-container'>
           Email
           <input
+          className='input lesserMargin'
             data-testid="email-input"
             type="email"
             placeholder="Email"
-            onChange={ (event) => setEmail(event.target.value) }
+            onChange={(event) => setEmail(event.target.value)}
           />
         </label>
-        <label htmlFor="password-input">
+        <label htmlFor="password-input" className='login-container'>
           Senha
           <input
+          className='input lesserMargin'
             data-testid="password-input"
             type="password"
             placeholder="Senha"
-            onChange={ (event) => setPassword(event.target.value) }
+            onChange={(event) => setPassword(event.target.value)}
           />
         </label>
         <button
+        className='button margin-top green'
           data-testid="signin-btn"
           type="button"
-          disabled={ !validEmail || password.length < passwordLength }
-          onClick={ () => fetchLogin(email, password).then((result) => handleLogin(result)) }
+          disabled={!validEmail || password.length < passwordLength}
+          onClick={() => fetchLogin(email, password).then((result) => handleLogin(result))}
         >
           ENTRAR
         </button>
         <Link to="/register">
-          <button data-testid="no-account-btn" type="button">
+          <button className="button margin-top brown" data-testid="no-account-btn" type="button">
             Ainda não tenho conta
           </button>
         </Link>
+        <div className="footer">
+          <p>Termos de uso Política de privacidade
+© 2021 TryBeer - Todos os direitos reservados.</p>
+        </div>
       </form>
     </section>
   );
