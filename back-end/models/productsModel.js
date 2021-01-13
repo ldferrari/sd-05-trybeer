236e087e6383fa258c4cd77ab43e3a8b0f88acf7
luiz-avelar <luiz.avelar25@gmail.com>
2021-01-13 17:42:06 -0300

    Requirement 8 backend

--- back-end/models/productsModel.js
+++ back-end/models/productsModel.js
@@ -1,7 +1,7 @@
 const db = require('./connection');
 
 const getProducts = async () => {
-  const products = await db.execute('SELECT name, price, url_image FROM products');
+  const products = await db.execute('SELECT id, name, price, url_image FROM products');
   return products;
 };
 
