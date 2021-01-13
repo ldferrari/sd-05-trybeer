807296ca007e71821bd7508519f751bd37d1b294
Christian Duessel <duessel@gmail.com>
2021-01-13 17:40:51 -0300

    client-orders-page

---
+++ front-end/src/pages/orders/index.js
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import { Redirect } from 'react-router-dom';
+import './index.css';
+import Header from '../../components/header';
+import Footer from '../../components/footer';
+import OrderCard from '../../components/ordersCard';
+// import CartButton from '../../components/cartButton';
+import { postGetOrders } from '../../services/requestAPI';
+
+const Orders = (props) => {
+  // const { history } = props;
+  const [theOrders, setOrders] = useState([]);
+  const theToken = localStorage.getItem('token');
+
+  useEffect(() => {
+    async function fetchOrders() {
+      const { data } = await postGetOrders(theToken);
+      setOrders(data);
+    }
+    fetchOrders();
+  }, [theToken]);
+
+  const logged = localStorage.getItem('token');
+
+  if (!logged) {
+    return <Redirect to="/login" />;
+  }
+
+  console.log(theOrders);
+  return (
+    <div className="Products">
+      <Header>Meus Pedidos</Header>
+      <div className="ordersList">
+        { theOrders.map((order, index) => <OrderCard key={ order.id } order={ order } index={ index } />) }
+        ,
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default Orders;
+
+Orders.propTypes = { history: PropTypes.instanceOf(Object).isRequired };
--- front-end/src/routes.js
+++ front-end/src/routes.js
@@ -3,6 +3,7 @@ import { Switch, Route, Redirect } from 'react-router-dom';
 
 import { Login, Profile, Products } from './pages';
 import Checkout from './pages/checkout';
+import Orders from './pages/orders';
 import Register from './pages/Register/Register';
 
 const Rotas = () => (
@@ -12,6 +13,8 @@ const Rotas = () => (
     <Route path="/products" component={ Products } />
     <Route exact path="/register" component={ Register } />
     <Route path="/checkout" component={ Checkout } />
+    <Route exact path="/orders" component={ Orders } />
+    {/* <Route exact path="/orders/:id" component={ OrderDetail } /> */}
     <Route exact path="/" component={ () => <Redirect to="/login" /> } />
   </Switch>
 );
