234946d738e90979533a7feddb61e7fa21cb3ae1
ederusena <eder.sena@live.com>
2021-01-13 15:27:10 -0300

    Requirement 10

--- front-end/src/pages/ProfileAdmin/index.js
+++ front-end/src/pages/ProfileAdmin/index.js
@@ -6,7 +6,6 @@ import { getProfileInfo } from '../../services/requestAPI';
 
 const PerfilAdmin = () => {
   const [name, setName] = useState('');
-  const [initialName, setInitialName] = useState('');
   const [email, setEmail] = useState('');
 
   useEffect(() => {
@@ -15,17 +14,21 @@ const PerfilAdmin = () => {
       const { data: { user } } = await getProfileInfo(token);
       setName(user.name);
       setEmail(user.email);
-      setInitialName(user.name);
     }
     asyncMe();
   }, []);
 
-  const nomes = name;
   return (
     <div className="App">
       <Header>Meu perfil</Header>
-      <h3 data-testid="profile-name">Nome: {name}</h3>
-      <h3 data-testid="profile-email">Email: {email}</h3>
+      <h3 data-testid="profile-name">
+        Nome:
+        {name}
+      </h3>
+      <h3 data-testid="profile-email">
+        Email:
+        {email}
+      </h3>
     </div>
 
   );
