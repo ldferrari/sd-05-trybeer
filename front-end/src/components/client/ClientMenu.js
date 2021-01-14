ae90267f98750f730992c24c8794b37046aab392
Larissa Palombo <64561249+larissapalombo@users.noreply.github.com>
2021-01-14 18:05:49 -0300

    Revert "Novo menu/3"

--- front-end/src/components/client/ClientMenu.js
+++ front-end/src/components/client/ClientMenu.js
@@ -1,7 +1,9 @@
 // ref https://medium.com/@renan.serverti/aprenda-frontend-como-criar-um-menu-toggle-responsivo-com-css-4f7f7a2ac07a
-import React, { useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 import PropTypes from 'prop-types';
+// import { TrybeerContext } from '../context/TrybeerContext';
 import '../../css/ClientMenu.css';
 
 function ClientMenu(props) {
@@ -16,20 +18,21 @@ function ClientMenu(props) {
       setClick(false);
     }
   }
-
+ 
   const left = (click) ? '0' : '-40%';
   const display = (click) ? 'block' : 'none';
+  
 
   return (
     <header className="menu-superior">
       <label className="top-hamburguer" data-testid="top-hamburguer" htmlFor="check">
         &#9776;
-        <input type="checkbox" id="check" onChange={ () => openClose() } />
+        <input type="checkbox" id="check" onChange={() => openClose()} />
       </label>
       <h1 className="top-title" data-testid="top-title">
         {title}
       </h1>
-      <div className="side-menu-container" style={ { left, display } }>
+      <div className="side-menu-container" style={{ left: left, display: display }} >
         <nav className="side-menu">
           <div className="btn-menu">
             <Link className="btn-side" data-testid="side-menu-item-products" to="/products">
