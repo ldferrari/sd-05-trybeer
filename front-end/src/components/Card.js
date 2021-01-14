837b646d6e2a17d834fb1a5b9edd4930602412f9
César Alves <alvescesar@hotmail.com>
2021-01-14 03:22:35 -0300

    Products page done 12/12

--- front-end/src/components/Card.js
+++ front-end/src/components/Card.js
@@ -1,13 +1,38 @@
-import React, { useState } from 'react';
+import React, { useEffect, useContext, useState } from 'react';
 import PropTypes from 'prop-types';
+import TryBeerContext from '../context/TryBeerContext';
 
 function Card(props) {
   const {
     index, image, name, price,
   } = props;
   const reais = price.replace('.', ',');
+  const { total, setTotal } = useContext(TryBeerContext);
   const zero = 0;
-  const [quantity, setQuantity] = useState(Math.abs(zero));
+  const [quantity, setQuantity] = useState(zero);
+
+  useEffect(() => {
+    setTotal(parseFloat(localStorage.getItem('totalPrice')) || zero);
+  }, [setTotal]);
+
+  const addProduct = () => {
+    if (quantity >= zero) {
+      setQuantity(quantity + 1);
+      localStorage.setItem('totalPrice', String(total + parseFloat(price)));
+      setTotal(total + parseFloat(price));
+    }
+  };
+
+  const removeProduct = () => {
+    if (quantity > zero) {
+      setQuantity(quantity > zero ? quantity - 1 : zero);
+      localStorage.setItem(
+        'totalPrice',
+        String(total > zero ? total - parseFloat(price) : zero),
+      );
+      setTotal(total > zero ? total - parseFloat(price) : zero);
+    }
+  };
 
   return (
     <section className="product-card">
@@ -22,15 +47,19 @@ function Card(props) {
       <button
         data-testid={ `${index}-product-minus` }
         type="button"
-        onClick={ () => setQuantity(quantity >= 1 ? quantity - 1 : zero) }
+        onClick={ () => removeProduct() }
       >
         -
       </button>
-      <span data-testid={ `${index}-product-qtd` }>{quantity}</span>
+      <span
+        data-testid={ `${index}-product-qtd` }
+      >
+        {quantity}
+      </span>
       <button
         data-testid={ `${index}-product-plus` }
         type="button"
-        onClick={ () => setQuantity(quantity + 1) }
+        onClick={ () => addProduct() }
       >
         +
       </button>
--- front-end/src/pages/Products.js
+++ front-end/src/pages/Products.js
@@ -1,11 +1,13 @@
 import React, { useContext, useEffect } from 'react';
+import { Link, Redirect } from 'react-router-dom';
+
 import TryBeerContext from '../context/TryBeerContext';
 import Header from '../components/Header';
 import Card from '../components/Card';
 import { getAllProducts } from '../services/ApiTrybeer';
 
 const Products = () => {
-  const { productsList, setProductList } = useContext(TryBeerContext);
+  const { productsList, setProductList, total } = useContext(TryBeerContext);
   const userData = JSON.parse(localStorage.getItem('user'));
   const token = userData && userData.token;
 
@@ -15,6 +17,7 @@ const Products = () => {
       .catch((err) => err);
   }, [setProductList, token]);
 
+  if (!token) return <Redirect to="/login" />;
   return (
     <section>
       <Header title="TryBeer" />
@@ -29,10 +32,26 @@ const Products = () => {
               price={ product.price }
             />
           )) }
-        {/* carrinho soma atual */}
       </section>
+      <Link to="/checkout">
+        <button
+          data-testid="checkout-bottom-btn"
+          type="button"
+          disabled={ !total }
+        >
+          Ver Carrinho
+        </button>
+      </Link>
+      <span data-testid="checkout-bottom-btn-value">
+        {new Intl.NumberFormat('pt-BR', {
+          style: 'currency',
+          currency: 'BRL',
+        }).format(total)}
+      </span>
     </section>
   );
 };
 
 export default Products;
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/NumberFormat
