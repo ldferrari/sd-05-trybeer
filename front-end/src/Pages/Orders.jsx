7e445f77749c150277a99779d59d4fcd30152412
Rafael Coper <rafael.coper@outlook.com>
2021-01-14 08:42:32 -0300

    Orders almost there

--- front-end/src/Pages/Orders.jsx
+++ front-end/src/Pages/Orders.jsx
@@ -1,12 +1,26 @@
-import PropTypes from 'prop-types';
-import React from 'react';
-import Header from '../Components/Header';
+import PropTypes from "prop-types";
+import React, { useEffect, useState } from "react";
+import { Redirect } from "react-router-dom";
+import Header from "../Components/Header";
+import SideBar from "../Components/SideBar";
+import { getClientOrder } from "../Redux/Services/index";
 
-function Orders({ history }) {
+function Orders() {
+  const [ordersList, setOrdersList] = useState(null);
+  const currentUser = JSON.parse(localStorage.getItem("user"));
+
+  useEffect(() => {
+    getClientOrder(id).then((data) => setOrdersList(data));
+  }, [id]);
+
+  if (!currentUser) return <Redirect to="/login" />;
+
   return (
     <div>
-      <Header pathname={ history.location.pathname } />
+      <Header pathname={history.location.pathname} />
       Cliente - Meus pedidos
+      <SideBar />
+      <p>{ordersList === null ? "Nenhum pedido" : "Loading"}</p>
     </div>
   );
 }
--- front-end/src/Redux/Services/index.js
+++ front-end/src/Redux/Services/index.js
@@ -17,20 +17,6 @@ const myInitWithBody = (data) => ({
   body: JSON.stringify(data),
 });
 
-// NÃO USAR ESTA SINTAXE *lint:
-
-// export const getProducts = () =>
-//   fetch(`${localhostURL}/products`, myInit).then((response) =>
-//     response
-//       .json()
-//       .then((json) =>
-//         response.ok ? Promise.resolve(json) : Promise.reject(json),
-//       ),
-//   );
-
-// Use esta aqui ou outra que não envolva retorno do retorno do ternário:
-
-// prettier-ignore
 export const getProducts = () => (
   fetch(`${localhostURL}/products`, myInit).then((response) => (
     response
@@ -39,6 +25,15 @@ export const getProducts = () => (
       .catch((err) => Promise.reject(err))
   ))
 );
+
+export const getClientOrder = (id) => (
+  fetch(`${localhostURL}/orders?id=${id}`, myInitWithBody.then((response) => (
+    response
+    .json()
+    .then((json) => Promise.resolve(json))
+    .catch((err) => Promise.reject(err))
+  )))
+)
 
 // prettier-ignore
 export const getUser = (data) => (
