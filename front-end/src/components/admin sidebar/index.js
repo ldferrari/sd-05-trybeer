c18fcacc23b4fce0009492437fbbbce9c5ae5851
Ishidor <kevinmendessilva@hotmail.com>
2021-01-15 18:45:26 -0300

    Renaming classes

--- front-end/src/components/admin sidebar/index.js
+++ front-end/src/components/admin sidebar/index.js
@@ -4,25 +4,25 @@ import { Link } from 'react-router-dom';
 import './index.css';
 
 const AdminSideBar = () => (
-  <aside className="sideBar admin-side-bar-container">
+  <aside className="adminSideBar admin-side-bar-container">
     <ul>
-      <li className="title">Trybeer</li>
-      <li className="menuBtnContainer">
-        <Link to="/admin/orders" className="menuBtn" data-testid="side-menu-item-orders">
+      <li className="adminTitle">Trybeer</li>
+      <li className="adminMenuBtnContainer">
+        <Link to="/admin/orders" className="adminMenuBtn" data-testid="side-menu-item-orders">
           Pedidos
         </Link>
       </li>
-      <li className="menuBtnContainer">
-        <Link to="/admin/profile" className="menuBtn" data-testid="side-menu-item-profile">
+      <li className="adminMenuBtnContainer">
+        <Link to="/admin/profile" className="adminMenuBtn" data-testid="side-menu-item-profile">
           Perfil
         </Link>
       </li>
     </ul>
     <ul>
-      <li className="menuBtnContainer">
+      <li className="adminMenuBtnContainer">
         <Link
           to="/"
-          className="menuBtn"
+          className="adminMenuBtn"
           data-testid="side-menu-item-logout"
           onClick={ () => { localStorage.removeItem('token'); } }
         >
--- front-end/src/pages/ProfileAdmin/index.js
+++ front-end/src/pages/ProfileAdmin/index.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Redirect } from 'react-router-dom';
 
-import Header from '../../components/header';
 import AdminSideBar from '../../components/admin sidebar';
 import './index.css';
 import { getProfileInfo } from '../../services/requestAPI';
