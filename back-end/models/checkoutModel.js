60171b6b80b0295d7f3f6a33188de4a527dc9351
pedrohcalado <pedrocalado22@gmail.com>
2021-01-15 15:22:51 -0300

    Lint

--- back-end/models/checkoutModel.js
+++ back-end/models/checkoutModel.js
@@ -1,23 +1,22 @@
 const database = require('./connection');
 
 const insertCheckout = async (
-  user_id,
-  total_price,
-  delivery_address,
-  delivery_number,
-  cart
+  userId,
+  totalPrice,
+  deliveryAddress,
+  deliveryNumber,
+  cart,
 ) => {
   const [
     checkout,
   ] = await database.execute(
-    'INSERT INTO sales (user_id, total_price, delivery_address, delivery_number, sale_date, status) VALUES (?, ?, ?, ?, now(), "Pendente")',
-    [user_id, total_price, delivery_address, delivery_number]
+    'INSERT INTO sales (userId, totalPrice, deliveryAddress, deliveryNumber, sale_date, status) VALUES (?, ?, ?, ?, now(), "Pendente")',
+    [userId, totalPrice, deliveryAddress, deliveryNumber],
   );
   for (let i = 0; i < cart.length; i++) {
-    await database.execute('INSERT INTO sales_products (sale_id, product_id, quantity) VALUES (?, ?, ?)', [checkout.insertId, cart[i].id, cart[i].quantity])
+    database.execute('INSERT INTO sales_products (sale_id, product_id, quantity) VALUES (?, ?, ?)', [checkout.insertId, cart[i].id, cart[i].quantity]);
   }
-  ;
-  return { message: 'Pedido Enviado', checkout }
+  return { message: 'Pedido Enviado', checkout };
 };
 
 module.exports = {
