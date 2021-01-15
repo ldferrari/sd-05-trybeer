cacb680bc886463a3b2cb43a234f1a0a5adfd2ab
Sidnei Ramos <sidneiramos02@gmail.com>
2021-01-15 01:01:49 -0300

    BackEnd da Tela de Checkout concluido

---
+++ back-end/controllers/checkoutController.js
@@ -0,0 +1,29 @@
+const { Router } = require('express');
+const checkoutService = require('../services/checkoutService');
+
+const checkout = Router();
+
+checkout.get('/', async (req, res) => {
+    const {
+      user_id,
+      total_price,
+      delivery_address,
+      delivery_number,
+      cart
+    } = req.body;
+  try {
+    const insertCheckout = await checkoutService.insertCheckout(
+      user_id,
+      total_price,
+      delivery_address,
+      delivery_number,
+      cart
+    );
+    res.status(200).json(insertCheckout);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Algo deu errado.' });
+  }
+});
+
+module.exports = checkout;
