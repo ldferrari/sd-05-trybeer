d1e7eadab5a7e5296c8ae45f2e5887df436ebe4e
Hugo J. De Moro Jr <hugo.urano@gmail.com>
2021-01-15 15:08:07 -0300

    criada pagina pedidos do cliente com respectiva rota

--- front-end/src/pages/Home.js
+++ front-end/src/pages/Home.js
@@ -6,7 +6,7 @@ import {
   Redirect,
 } from 'react-router-dom';
 import {
-  Login, Register, Products, Orders, Profile,
+  Login, Register, Products, Orders, Profile, ClientOrders,
 } from '.';
 
 const Home = () => (
@@ -17,6 +17,7 @@ const Home = () => (
       </Route>
       <Route exact path="/login" component={ Login } />
       <Route exact path="/products" component={ Products } />
+      <Route exact path="/orders" component={ ClientOrders } />
       <Route exact path="/register" component={ Register } />
       <Route exact path="/profile" component={ Profile } />
       <Route exact path="/admin/profile" component={ Profile } />
