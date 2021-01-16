991984c7072953b937076c60cfbd408eb8cb1e3f
Sidnei Ramos <sidneiramos02@gmail.com>
2021-01-16 19:24:57 -0300

    Tela de detalhe de pedido pronta

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -9,20 +9,25 @@ import AdminOrderPage from './pages/admin/AdminOrderPage';
 import ClientCheckoutPage from './pages/client/ClientCheckoutPage';
 import ClientProfilePage from './pages/client/ClientProfilePage';
 import './css/client/app.css';
+import ClientDetailsOrderPage from './pages/client/ClientDetailsOrderPage';
 
 function App() {
   return (
     <div className="App">
       <Switch>
-        <Route path="/admin/orders" component={ AdminOrderPage } />
-        <Route path="/admin/profile" component={ AdminProfilePage } />
-        <Route path="/login" component={ LoginPage } />
-        <Route path="/register" component={ RegisterPage } />
-        <Route path="/products" component={ ClientProductPage } />
-        <Route path="/profile" component={ ClientProfilePage } />
-        <Route path="/orders" component={ ClientMeusPedidos } />
-        <Route path="/checkout" component={ ClientCheckoutPage } />
-        <Route path="/" component={ () => <Redirect to="/login" /> } />
+        <Route path="/admin/orders" component={AdminOrderPage} />
+        <Route path="/admin/profile" component={AdminProfilePage} />
+        <Route path="/login" component={LoginPage} />
+        <Route path="/register" component={RegisterPage} />
+        <Route path="/products" component={ClientProductPage} />
+        <Route path="/profile" component={ClientProfilePage} />
+        <Route
+          path="/orders/:id"
+          render={(props) => <ClientDetailsOrderPage {...props} />}
+        />
+        <Route path="/orders" component={ClientMeusPedidos} />
+        <Route path="/checkout" component={ClientCheckoutPage} />
+        <Route path="/" component={() => <Redirect to="/login" />} />
       </Switch>
     </div>
   );
--- front-end/src/pages/client/ClientDetailsOrderPage.jsx
+++ front-end/src/pages/client/ClientDetailsOrderPage.jsx
@@ -1,16 +1,57 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Redirect } from 'react-router-dom';
-// import Menu from '../../components/client/Menu';
+import Menu from '../../components/client/Menu';
+import fetchOrderDetails from '../../services/client/fetchOrderDetails';
 
-export default function ClientDetailsOrderPage() {
+export default function ClientDetailsOrderPage(props) {
+  const [order, setOrder] = useState('');
   const token = localStorage.getItem('token') || null;
+  console.log(order);
+  const id = props.match.params.id;
+  const zero = 0;
+  const dois = 2;
+
+  useEffect(() => {
+    fetchOrderDetails(id).then((data) => setOrder(data));
+  }, [id]);
 
   if (!token) return <Redirect to="/login" />;
   return (
     <div>
-      {/* <Menu title="Cliente - Detalhes de Pedido" /> */}
+      <Menu title="Cliente - Detalhes de Pedido" />
       <div data-testid="0-order-card-container">
-        <p data-testid="0-order-number">Pedido 00</p>
+        <div>
+          <span data-testid="order-number">
+            {order && `Pedido ${order.orderDetail[0].sale_id}`}
+          </span>
+          <span data-testid="order-date">
+            {order && order.orderDetail[0].sale_date}
+          </span>
+        </div>
+        {order &&
+          order.orderDetail.map((product, index) => (
+            <div>
+              <span>{`${index + 1}`}</span>
+              <span
+                data-testid={`${index}-product-name`}
+              >{`${product.name}`}</span>
+              <span
+                data-testid={`${index}-product-qtd`}
+              >{`${product.quantity}`}</span>
+              <span
+                data-testid={`${index}-product-total-value`}
+              >{`R$ ${product.total.toFixed(dois).replace('.', ',')}`}</span>
+            </div>
+          ))}
+        {order && (
+          <div data-testid="order-total-value">
+            {`R$ ${order.orderDetail
+              .reduce((total, product) => total + product.total, zero)
+              .toFixed(dois)
+              .replace('.', ',')}
+            `}
+          </div>
+        )}
       </div>
     </div>
   );
