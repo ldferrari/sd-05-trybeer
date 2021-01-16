e69c86afc40a121771e8752230098ee3234efb55
Andrevlc <andrevezi@gmail.com>
2021-01-15 21:21:16 -0300

    Requisite 12 done

--- front-end/src/components/OrdersAdmDetails.js
+++ front-end/src/components/OrdersAdmDetails.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { updateStatus, getAdmOrderById } from '../services/api';
+import Header from './Header';
 
 const OrdersAdminDetails = (props) => {
   const zero = 0;
@@ -20,6 +21,7 @@ const OrdersAdminDetails = (props) => {
 
   return (
     <div>
+      <Header>Menu</Header>
       <table>
         <caption data-testid="order-number">
           {`Pedido ${id}`}
@@ -34,14 +36,14 @@ const OrdersAdminDetails = (props) => {
         {order.map((product, index) => (
           <tr key={ product.name }>
             <td data-testid={ `${index}-product-name` }>{product.name}</td>
-            <td>{ `R$ ${product.price}` }</td>
-            <td data-testid={ `${index}-order-qtd` }>{product.quantity}</td>
-            <td data-testid={ `${index}-order-total-value` }>{`R$ ${(product.price * product.quantity).toFixed(two).replace('.', ',')}`}</td>
+            <td data-testid={ `${index}-order-unit-price` }>{ `(R$ ${product.price.replace('.', ',')})` }</td>
+            <td data-testid={ `${index}-product-qtd` }>{product.quantity}</td>
+            <td data-testid={ `${index}-product-total-value` }>{`R$ ${(product.price * product.quantity).toFixed(two).replace('.', ',')}`}</td>
           </tr>
         ))}
       </table>
       <p data-testid="order-total-value">{order[0] ? `R$ ${String(order[0].total_price).replace('.', ',')}` : zero}</p>
-      {status === 'Pendente' ? <button type="button" data-testid="mark-as-delivered-btn" onClick={ handleClick }>Marcar pedido como entregue</button> : null}
+      {status === 'Pendente' ? <button type="button" data-testid="mark-as-delivered-btn" onClick={ handleClick }>Marcar como entregue</button> : zero}
     </div>
   );
 };
