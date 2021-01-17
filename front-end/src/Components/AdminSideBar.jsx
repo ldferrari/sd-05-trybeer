261cb2f250651be2abe04f13879b40963899e7fe
Paulo-Dandrea <prog.dandrea@gmail.com>
2021-01-17 13:26:46 -0300

    admin side bar

--- front-end/src/Components/AdminSideBar.jsx
+++ front-end/src/Components/AdminSideBar.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const AdminSideBar = () => (<div>
-  {' '}
-  admin side bar
+const sideBarStyle = {
+  background: 'var(--dark)',
+  color: 'var(--white)',
+  height: '100vh',
+  width: '10vw',
+  display: 'flex',
+  'flex-direction': 'column',
+  'text-align': 'center',
+};
 
-  <Link to="/admin/profile" data-testid="side-menu-item-profile">Admin Profile</Link>
-
-</div>);
+const AdminSideBar = () => (
+  <div className="admin-side-bar-container" style={ sideBarStyle }>
+    <Link style={ { color: 'white' } } to="/admin/orders" data-testid="side-menu-item-orders">
+      Meus Pedidos
+    </Link>
+    <Link style={ { color: 'white' } } to="/admin/profile" data-testid="side-menu-item-profile">
+      Perfil
+    </Link>
+    <Link style={ { color: 'white' } } to="/admin/profile" data-testid="side-menu-item-logout">
+      Sair
+    </Link>
+  </div>
+);
 
 export default AdminSideBar;
