e2ff66e3f0303da96329f57c8f9d1d3aba41ed2c
César Alves <alvescesar@hotmail.com>
2021-01-17 15:02:15 -0300

    Fixed page crashes. I think...

--- front-end/src/pages/OrderDetails.js
+++ front-end/src/pages/OrderDetails.js
@@ -14,22 +14,22 @@ export default function OrderDetails(props) {
     },
   } = props;
   const [orders, setOrders] = useState([]);
-  const [products, setProducts] = useState([]);
   const [date, setDate] = useState('');
   const [total, setTotal] = useState(zero);
-  const cart = JSON.parse(localStorage.getItem('cart'));
   const userData = JSON.parse(localStorage.getItem('user'));
+  const products = JSON.parse(localStorage.getItem('cart'));
   const email = userData && userData.user && userData.user.email;
   const token = userData && userData.token;
 
   useEffect(() => {
     fetchOrderId(email).then((sales) => {
-      setDate(sales[0].sale_date);
-      setTotal(sales[0].total_price);
-      setProducts(cart);
-      return setOrders(sales);
+      if (sales.length > zero) {
+        setDate(sales[0].sale_date);
+        setTotal(sales[0].total_price);
+        setOrders(sales);
+      }
     });
-  }, [email, cart]);
+  }, [email]);
 
   if (!token) return <Redirect to="/login" />;
   return (
@@ -40,14 +40,14 @@ export default function OrderDetails(props) {
         {orders.map((product, index) => (
           <section key={ product.id }>
             <h3 data-testid={ `${index}-product-name` }>
-              {products[index].name}
+              {products[index] && products[index].name}
             </h3>
-            <span>{`R$ ${products[index].price}`}</span>
+            <span>{products[index] && `R$ ${products[index].price}`}</span>
             <span data-testid={ `${index}-product-qtd` }>
-              {products[index].quantity}
+              {products[index] && products[index].quantity}
             </span>
             <span data-testid={ `${index}-product-total-value` }>
-              {`R$ ${(products[index].price * products[index].quantity)
+              {products[index] && `R$ ${(products[index].price * products[index].quantity)
                 .toFixed(decimals)
                 .replace('.', ',')}`}
             </span>
