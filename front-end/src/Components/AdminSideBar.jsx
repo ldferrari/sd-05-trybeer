d2e2ba8eb8271fc2cff7d18f5b9a3eb556defe8c
Paulo-Dandrea <prog.dandrea@gmail.com>
2021-01-17 14:06:38 -0300

    admin side bar passando no teste

--- front-end/src/Components/AdminSideBar.jsx
+++ front-end/src/Components/AdminSideBar.jsx
@@ -1,5 +1,8 @@
+import PropTypes from 'prop-types';
 import React from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { clear } from '../Redux/Actions/user';
 
 const sideBarStyle = {
   background: 'var(--dark)',
@@ -7,22 +10,44 @@ const sideBarStyle = {
   height: '100vh',
   width: '10vw',
   display: 'flex',
-  'flex-direction': 'column',
-  'text-align': 'center',
+  flexDirection: 'column',
+  textAlign: 'center',
+  float: 'left',
 };
 
-const AdminSideBar = () => (
+const AdminSideBar = ({ logout }) => (
   <div className="admin-side-bar-container" style={ sideBarStyle }>
-    <Link style={ { color: 'white' } } to="/admin/orders" data-testid="side-menu-item-orders">
+    <Link
+      style={ { color: 'white' } }
+      to="/admin/orders"
+      data-testid="side-menu-item-orders"
+    >
       Meus Pedidos
     </Link>
-    <Link style={ { color: 'white' } } to="/admin/profile" data-testid="side-menu-item-profile">
+    <Link
+      style={ { color: 'white' } }
+      to="/admin/profile"
+      data-testid="side-menu-item-profile"
+    >
       Perfil
     </Link>
-    <Link style={ { color: 'white' } } to="/admin/profile" data-testid="side-menu-item-logout">
+    <Link
+      style={ { color: 'white' } }
+      onClick={ () => logout() }
+      to="/login"
+      data-testid="side-menu-item-logout"
+    >
       Sair
     </Link>
   </div>
 );
 
-export default AdminSideBar;
+AdminSideBar.propTypes = {
+  logout: PropTypes.func.isRequired,
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  logout: () => dispatch(clear()),
+});
+
+export default connect(null, mapDispatchToProps)(AdminSideBar);
