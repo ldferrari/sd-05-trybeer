f387b0112d73bc01f1a5c98c31bf1e1cd42eb2ce
Paulo-Dandrea <prog.dandrea@gmail.com>
2021-01-17 12:55:59 -0300

    sidebar lint

--- front-end/src/Components/AdminSideBar.jsx
+++ front-end/src/Components/AdminSideBar.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const AdminSideBar = () => {
-  return ( <div> admin side bar
+const AdminSideBar = () => (<div>
+  {' '}
+  admin side bar
 
-<Link to="/admin/profile" data-testid="side-menu-item-profile">Admin Profile</Link>
+  <Link to="/admin/profile" data-testid="side-menu-item-profile">Admin Profile</Link>
 
-  </div> );
-}
- 
+</div>);
+
 export default AdminSideBar;
