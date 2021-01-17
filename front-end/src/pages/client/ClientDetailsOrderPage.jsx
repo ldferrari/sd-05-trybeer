e634c2e228965d7c787199b0c8cecd6a5280820a
ferrari <luisdiasferrari@gmail.com>
2021-01-17 11:59:04 -0300

    css

--- front-end/src/pages/client/ClientDetailsOrderPage.jsx
+++ front-end/src/pages/client/ClientDetailsOrderPage.jsx
@@ -3,13 +3,14 @@ import { Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import Menu from '../../components/client/Menu';
 import fetchOrderDetails from '../../services/client/fetchOrderDetails';
+import '../../css/client/clientDetailsOrderPage.css';
 
 export default function ClientDetailsOrderPage(props) {
   const [order, setOrder] = useState('');
   const token = localStorage.getItem('token') || null;
   const zero = 0;
   const dois = 2;
-
+  
   useEffect(() => {
     fetchOrderDetails(props.match.params.id).then((data) => setOrder(data));
   }, []);
@@ -17,9 +18,9 @@ export default function ClientDetailsOrderPage(props) {
   if (!token) return <Redirect to="/login" />;
   return (
     <div>
-      <Menu title="Cliente - Detalhes de Pedido" />
-      <div data-testid="0-order-card-container">
-        <div>
+      <Menu title="Detalhes de Pedido" />
+      <div data-testid="0-order-card-container" className="datailsBody">
+        <div className="datailPedido">
           <span data-testid="order-number">
             {order && `Pedido ${order.orderDetail[0].sale_id}`}
           </span>
@@ -29,23 +30,23 @@ export default function ClientDetailsOrderPage(props) {
         </div>
         {order && (
           order.orderDetail.map((product, index) => (
-            <div key="fechalint">
-              <span key="indice">{ `${index + 1}` }</span>
+            <div key="fechalint" className="datailCard">
+              {/* <span key="indice">{ `${index + 1}` }</span> */}
               <span data-testid={ `${index}-product-name` }>
                 { `${product.name}` }
               </span>
               <span data-testid={ `${index}-product-qtd` }>
-                { `${product.quantity}` }
+                { `Qtd: ${product.quantity}` }
               </span>
-              <span data-testid={ `${index}-product-total-value` }>
+              <span data-testid={ `${index}-product-total-value` } className="datailValue">
                 { `R$ ${product.total.toFixed(dois).replace('.', ',')}` }
               </span>
             </div>
           ))
         ) }
         {order && (
-          <div data-testid="order-total-value">
-            {`R$ ${order.orderDetail
+          <div data-testid="order-total-value" className="detailsTotal">
+            {`Total R$ ${order.orderDetail
               .reduce((total, product) => total + product.total, zero)
               .toFixed(dois)
               .replace('.', ',')}
