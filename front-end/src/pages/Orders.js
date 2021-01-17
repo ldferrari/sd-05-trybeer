00a99676fa03c45614015befeec115104a98b7f5
César Alves <alvescesar@hotmail.com>
2021-01-17 07:08:41 -0300

    Fixed broken tests

--- front-end/src/pages/Orders.js
+++ front-end/src/pages/Orders.js
@@ -3,20 +3,21 @@ import { Redirect } from 'react-router-dom';
 
 import Header from '../components/Header';
 import ClientOrderCard from '../components/ClientOrderCard';
-import AdminOrderCard from '../components/AdminOrderCard';
+// import AdminOrderCard from '../components/AdminOrderCard';
 
 export default function Orders() {
   const userData = JSON.parse(localStorage.getItem('user'));
-  const role = userData && userData.user && userData.user.role;
+  // const role = userData && userData.user && userData.user.role;
   const token = userData && userData.token;
 
   if (!token) return <Redirect to="/login" />;
-  return role === 'administrator' ? (
-    <section>
-      <Header title="Pedidos Pendentes" />
-      <AdminOrderCard token={ token } />
-    </section>
-  ) : (
+  // return role === 'administrator' ? (
+  //   <section>
+  //     <Header title="Pedidos Pendentes" />
+  //     <AdminOrderCard token={ token } />
+  //   </section>
+  // ) : (
+  return (
     <section>
       <Header title="Meus Pedidos" />
       <ClientOrderCard />
