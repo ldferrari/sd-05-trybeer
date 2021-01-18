31a88dc18c5146af9cb438c940526af0cbba8583
Mau <mauricio.sdjr@gmail.com>
2021-01-17 23:27:29 -0300

    eslint

--- front-end/src/pages/admin/AdminSaleDetailsPage.jsx
+++ front-end/src/pages/admin/AdminSaleDetailsPage.jsx
@@ -28,44 +28,44 @@ export default function AdminSaleDetailsPage(props) {
       <MenuAdm />
       {/* <div className="bodyMargin"> */}
       <div data-testid="order-number">
-          {saleDetails && `Pedido ${saleDetails[0].sale_id}`}
-        </div>
+        {saleDetails && `Pedido ${saleDetails[0].sale_id}`}
+      </div>
       <div data-testid="order-status">
-          {saleDetails && `${saleDetails[0].status}`}
-        </div>
+        {saleDetails && `${saleDetails[0].status}`}
+      </div>
       {saleDetails && (saleDetails.map((product, index) => (
-          <div key="eslint">
-            <span data-testid={ `${index}-product-qtd` }>
-              {`${product.quantity}`}
-            </span>
-            <span data-testid={ `${index}-product-name` }>
-              {`${product.name}`}
-            </span>
-            <span data-testid={ `${index}-product-total-value` }>
-              {`R$ ${product.total.toFixed(dois).replace('.', ',')}`}
-            </span>
-            <span data-testid={ `${index}-order-unit-price` }>
-              {`(R$ ${product.price.replace('.', ',')})`}
-            </span>
-          </div>
-        )))}
-      <div data-testid="order-total-value">
-          {saleDetails && (`Total: R$ ${saleDetails[0].total_price.replace('.', ',')}`)}
+        <div key="eslint">
+          <span data-testid={ `${index}-product-qtd` }>
+            {`${product.quantity}`}
+          </span>
+          <span data-testid={ `${index}-product-name` }>
+            {`${product.name}`}
+          </span>
+          <span data-testid={ `${index}-product-total-value` }>
+            {`R$ ${product.total.toFixed(dois).replace('.', ',')}`}
+          </span>
+          <span data-testid={ `${index}-order-unit-price` }>
+            {`(R$ ${product.price.replace('.', ',')})`}
+          </span>
         </div>
+      )))}
+      <div data-testid="order-total-value">
+        {saleDetails && (`Total: R$ ${saleDetails[0].total_price.replace('.', ',')}`)}
+      </div>
       {saleDetails && (saleDetails[0].status === 'Pendente' ? (
-          <button
-            data-testid="mark-as-delivered-btn"
-            type="button"
-            onClick={ () => handleClick(id) }
-          >
-            Marcar como entregue
-          </button>
-        ) : null)}
+        <button
+          data-testid="mark-as-delivered-btn"
+          type="button"
+          onClick={ () => handleClick(id) }
+        >
+          Marcar como entregue
+        </button>
+      ) : null)}
       {/* </div> */}
     </div>
   );
 }
 
-ClientDetailsOrderPage.propTypes = {
+AdminSaleDetailsPage.propTypes = {
   id: PropTypes.string,
 };
