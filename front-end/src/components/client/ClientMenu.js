b550bb1b8c0ee23a808c62efcef49cc608fe3d73
virginia-duca <virginiambd.bio@gmail.com>
2021-01-18 09:36:34 -0300

    side menu

--- front-end/src/components/client/ClientMenu.js
+++ front-end/src/components/client/ClientMenu.js
@@ -1,39 +1,35 @@
 // ref https://medium.com/@renan.serverti/aprenda-frontend-como-criar-um-menu-toggle-responsivo-com-css-4f7f7a2ac07a
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-// import { useState } from 'react';
 import PropTypes from 'prop-types';
-// import { TrybeerContext } from '../context/TrybeerContext';
 import '../../css/ClientMenu.css';
 
 function ClientMenu(props) {
-  // const [click, setClick] = useState(false);
+  const [click, setClick] = useState(false);
   const { title } = props;
 
   function openClose() {
     if (document.getElementById('check').checked) {
-      document.getElementsByClassName('side-menu-container')[0].style.left = '0%';
+      setClick(true);
     }
     if (!document.getElementById('check').checked) {
-      document.getElementsByClassName('side-menu-container')[0].style.left = '-40%';
+      setClick(false);
     }
   }
 
+  const left = (click) ? '0' : '-40%';
+  const display = (click) ? 'block' : 'none';
+
   return (
-    <div className="menu-header orange-background">
-      <header>
-      <div className="header-content">
+    <header className="menu-superior">
       <label className="top-hamburguer" data-testid="top-hamburguer" htmlFor="check">
         &#9776;
         <input type="checkbox" id="check" onChange={ () => openClose() } />
       </label>
-      <h4 className="top-title" data-testid="top-title" className="white-text">
+      <h1 className="top-title" data-testid="top-title">
         {title}
-      </h4>
-      <div></div>
-      </div>
-    </header>
-      <div className="side-menu-container orange-background">
+      </h1>
+      <div className="side-menu-container" style={ { left, display } }>
         <nav className="side-menu">
           <div className="btn-menu">
             <Link className="btn-side" data-testid="side-menu-item-products" to="/products">
@@ -51,7 +47,7 @@ function ClientMenu(props) {
           </div>
         </nav>
       </div>
-    </div>
+    </header>
   );
 }
 
